3a1d4cde542132ebdccb8b7dc4c8ee6bb63b62f5
Archit Patel <arcpatel58@gmail.com>
2023-05-28 17:01:13 +0100

    added login and register components

---
+++ resources/views/components/layout/login.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>{{ $title ?? 'AdminLTE 3 | Blank Page' }}</title>
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="{{ URL::asset('plugins/fontawesome-free/css/all.min.css') }}">
+    <!-- icheck bootstrap -->
+    <link rel="stylesheet" href="{{ URL::asset('plugins/icheck-bootstrap/icheck-bootstrap.min.css') }}">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="{{ URL::asset('dist/css/adminlte.min.css') }}">
+
+    {{ $styles ?? '' }}
+
+</head>
+
+<body class="hold-transition login-page">
+
+    {{ $slot }}
+    <!-- /.login-box -->
+
+    <!-- jQuery -->
+    <script src="{{ URL::asset('plugins/jquery/jquery.min.js') }}"></script>
+    <!-- Bootstrap 4 -->
+    <script src="{{ URL::asset('plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <!-- AdminLTE App -->
+    <script src="{{ URL::asset('dist/js/adminlte.min.js') }}"></script>
+
+    {{ $scripts ?? '' }}
+
+</body>
+
+</html>
